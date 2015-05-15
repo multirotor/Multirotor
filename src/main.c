6801443b76159060e9d9f613586a4f01b1a09ef6
Martin Pruscha <martin.pruscha@afconsult.com>
2015-05-15 21:16:14 +0200

    moved CMakeLists.txt again because of Eclipse porject generation problems. I2C works now with the MPU9150 board, but needs to be cleaned up. USART seems to work, but also has to be cleaned up

--- src/main.c
+++ src/main.c
@@ -23,48 +23,239 @@
 //#define LED_OFF() GPIOA->BSRRH |= (1 << 5)
 #define LED_TOGGLE() GPIOA->ODR ^= (1 << 5)
 
+#define MPU9150_WHO_AM_I           0x75   // R
+
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/i2c.h>
+#include <libopencm3/stm32/flash.h>
+#include <libopencm3/stm32/usart.h>
 
-//Quick hack, approximately 1ms delay
-void ms_delay(int ms) {
-   while (ms-- > 0) {
-      volatile int x=5971;
-      while (x-- > 0)
-         __asm("nop");
-   }
+
+/* for USB to work, we need a USB clock of 48MHz */
+const clock_scale_t hsi_8mhz_3v3 = {
+	 /* 84MHz */
+		.pllm = 16,
+		.plln = 336,
+		.pllp = 4,
+		.pllq = 7,
+		.hpre = RCC_CFGR_HPRE_DIV_NONE,
+		.ppre1 = RCC_CFGR_PPRE_DIV_4,
+		.ppre2 = RCC_CFGR_PPRE_DIV_2,
+		.power_save = 1,
+		.flash_config = FLASH_ACR_ICE | FLASH_ACR_DCE |
+				FLASH_ACR_LATENCY_3WS,
+		.apb1_frequency = 21000000,
+		.apb2_frequency = 42000000,
+};
+
+void rcc_clock_setup_hsi_3v3(const clock_scale_t *clock)
+{
+	/* Enable internal high-speed oscillator. */
+	rcc_osc_on(HSI);
+	rcc_wait_for_osc_ready(HSI);
+
+	/* Select HSI as SYSCLK source. */
+	rcc_set_sysclk_source(RCC_CFGR_SW_HSI);
+
+//	/* Enable/disable high performance mode */
+//	if (!clock->power_save) {
+//		pwr_set_vos_scale(SCALE1);
+//	} else {
+//		pwr_set_vos_scale(SCALE2);
+//	}
+
+	/*
+	 * Set prescalers for AHB, ADC, ABP1, ABP2.
+	 * Do this before touching the PLL (TODO: why?).
+	 */
+	rcc_set_hpre(clock->hpre);
+	rcc_set_ppre1(clock->ppre1);
+	rcc_set_ppre2(clock->ppre2);
+
+	rcc_set_main_pll_hsi(clock->pllm, clock->plln,
+			clock->pllp, clock->pllq);
+
+	/* Enable PLL oscillator and wait for it to stabilize. */
+	rcc_osc_on(PLL);
+	rcc_wait_for_osc_ready(PLL);
+
+	/* Configure flash settings. */
+	flash_set_ws(clock->flash_config);
+
+	/* Select PLL as SYSCLK source. */
+	rcc_set_sysclk_source(RCC_CFGR_SW_PLL);
+
+	/* Wait for PLL clock to be selected. */
+	rcc_wait_for_sysclk_status(PLL);
+
+	/* Set the peripheral clock frequencies used. */
+	rcc_apb1_frequency = clock->apb1_frequency;
+	rcc_apb2_frequency = clock->apb2_frequency;
+
+	/* Disable internal high-speed oscillator. */
+	//rcc_osc_off(HSI);
 }
+
+
+int clock_setup()
+{
+	/* Setup clock 84Mh */
+	rcc_clock_setup_hsi_3v3(&hsi_8mhz_3v3);
+
+	/* setup clock for LEDs and USART */
+	rcc_periph_clock_enable(RCC_GPIOA);
+
+	/* Enable clocks for USART2. */
+	rcc_periph_clock_enable(RCC_USART2);
+
+	/* must be done here?! */
+	rcc_periph_clock_enable(RCC_I2C1);
+	rcc_periph_clock_enable(RCC_GPIOB);
+
+}
+
+int i2c_setup()
+{
+
+//	i2c_peripheral_disable(I2C1);
+
+	gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO8 | GPIO9);
+	gpio_set_af(GPIOB, GPIO_AF4, GPIO8 | GPIO9);
+	gpio_set_output_options(GPIOB, GPIO_OTYPE_OD, GPIO_OSPEED_2MHZ, GPIO8 | GPIO9);
+
+	/* i2c peripheral runs on APB1, and we set the APB1 frequency to 21MHz in clock_setup() */
+	i2c_set_clock_frequency(I2C1, I2C_CR2_FREQ_21MHZ);
+
+	i2c_set_fast_mode(I2C1);
+
+	/* we would like to have 400kHz, that is 21Mhz/400kHz=52.5. Closest: 53, gives 396226Hz~400kHz i2c clock */
+	i2c_set_ccr(I2C1, 30);
+
+
+	i2c_peripheral_enable(I2C1);
+
+
+
+}
+
+static void usart_setup(void)
+{
+	/* Setup USART2 parameters. */
+	usart_set_baudrate(USART2, 115200);
+	usart_set_databits(USART2, 8);
+	usart_set_stopbits(USART2, USART_STOPBITS_1);
+	usart_set_mode(USART2, USART_MODE_TX);
+	usart_set_parity(USART2, USART_PARITY_NONE);
+	usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+
+	/* Finally enable the USART. */
+	usart_enable(USART2);
+}
+
+
+static void gpio_setup(void)
+{
+	/* gpio setup for blink LED */
+	gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO5);
+
+	/* Setup GPIO pins for USART2 transmit. */
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2);
+
+	/* Setup USART2 TX pin as alternate function. */
+	gpio_set_af(GPIOA, GPIO_AF7, GPIO2);
+
+
+
+}
+
+
 int main() {
 
 	int i = 0;
+	int j = 0;
+	int c = 0;
 
-	rcc_periph_clock_enable(RCC_GPIOA);
+	uint8_t temperature;
 
-	gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO5);
+	clock_setup();
+	usart_setup();
+	i2c_setup();
+	gpio_setup();
+
 
 	while(1)
 	{	
 		/* Using API function gpio_toggle(): */
 		gpio_toggle(GPIOA, GPIO5);	/* LED on/off */
-		for (i = 0; i < 1000000; i++) {	/* Wait a bit. */
+
+//		usart_send_blocking(USART2, c + '0'); /* USART2: Send byte. */
+//		c = (c == 9) ? 0 : c + 1;	/* Increment c. */
+//		if ((j++ % 80) == 0) {		/* Newline after line full. */
+//			usart_send_blocking(USART2, '\r');
+//			usart_send_blocking(USART2, '\n');
+//		}
+
+		i2c_peripheral_enable(I2C1);
+
+		i2c_send_start(I2C1);
+
+
+		/* Waiting for START is send and switched to master mode. */
+		while (!((I2C_SR1(I2C1) & I2C_SR1_SB)
+			& (I2C_SR2(I2C1) & (I2C_SR2_MSL | I2C_SR2_BUSY))));
+
+		i2c_send_7bit_address(I2C1, 0x68, I2C_WRITE);
+
+
+		/* Waiting for address is transferred. */
+		while (!(I2C_SR1(I2C1) & I2C_SR1_ADDR));
+
+		I2C_SR2(I2C1);
+
+
+		i2c_send_data(I2C1, 0x6B); /* temperature register */
+
+		while (!(I2C_SR1(I2C1) & (I2C_SR1_BTF | I2C_SR1_TxE)));
+
+		/* Send START condition. */
+		i2c_send_start(I2C1);
+
+		/* Waiting for START is send and switched to master mode. */
+		while (!((I2C_SR1(I2C1) & I2C_SR1_SB)
+			& (I2C_SR2(I2C1) & (I2C_SR2_MSL | I2C_SR2_BUSY))));
+
+		/* Say to what address we want to talk to. */
+		i2c_send_7bit_address(I2C1, 0x68, I2C_READ);
+
+		/* Waiting for address is transferred. */
+		while (!(I2C_SR1(I2C1) & I2C_SR1_ADDR));
+
+		I2C_SR2(I2C1);
+
+		/* Cleaning I2C_SR1_ACK. */
+		I2C_CR1(I2C1) &= ~I2C_CR1_ACK;
+
+//		i2c_get_data()
+
+		/* Now the slave should begin to send us the first byte. Await BTF. */
+//		while (!(I2C_SR1(I2C1) & I2C_SR1_BTF))
+//		{
+		temperature = (uint8_t)(I2C_DR(I2C1) << 8); /* MSB */
+
+//		}
+
+		i2c_peripheral_disable(I2C1);
+
+		usart_send_blocking(USART2, temperature);
+		usart_send_blocking(USART2, '\r');
+		usart_send_blocking(USART2, '\n');
+
+		for (i = 0; i < 4000000; i++) {	/* Wait a bit. */
 			__asm__("nop");
 		}
 	}
-	// Enable GPIOA clock.
-	// RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-	// Configure GPIOA pin 5 as output.
-        //GPIOA->MODER |= (1 << (LED_PIN << 1));
-	// Configure GPIOA pin 5 in max speed.
-	//GPIOA->OSPEEDR |= (3 << (LED_PIN << 1));
-	// Turn on the LED.
-	//LED_ON();
-  // Blink LED.
-//TEST
-//  while(1) {
-//    ms_delay(200);
-	//LED_TOGGLE();
-//  }
 
 
 }
