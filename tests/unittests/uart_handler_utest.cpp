ca43ded073dc5e533475c11a5c1abc33daa91eec
Monika Lukow <monika.lukow@gmail.com>
2015-11-24 22:57:20 +0100

    Tests: Uart_Handler tests added

--- tests/unittests/uart_handler_utest.cpp
+++ tests/unittests/uart_handler_utest.cpp
@@ -48,3 +48,73 @@ TEST_F(GetRxData, ReadMaximal) {
   EXPECT_EQ(65535, uart_handler.get_rx_data());
 }
 
+
+/***** Uart_Handler::receive() *****/
+
+class Receive : public ::testing::Test {
+protected:
+    virtual void SetUp() {
+        mocked_usart = new MockUsart();
+    }
+
+    virtual void TearDown() {
+        delete mocked_usart;
+    }
+};
+
+
+TEST_F(Receive, ReceiveZero) {
+    Uart_Handler uart_handler;
+
+    ON_CALL(*mocked_usart, usart_recv_blocking_invoke(USART2)).WillByDefault(Return(0));
+    EXPECT_CALL(*mocked_usart, usart_recv_blocking_invoke(USART2)).Times(1);
+
+    uart_handler.receive();
+}
+
+TEST_F(Receive, ReceivePositive) {
+    Uart_Handler uart_handler;
+
+    ON_CALL(*mocked_usart, usart_recv_blocking_invoke(USART2)).WillByDefault(Return(98));
+    EXPECT_CALL(*mocked_usart, usart_recv_blocking_invoke(USART2)).Times(1);
+
+    uart_handler.receive();
+}
+
+TEST_F(Receive, ReceiveMaximal) {
+    Uart_Handler uart_handler;
+
+    ON_CALL(*mocked_usart, usart_recv_blocking_invoke(USART2)).WillByDefault(Return(65535));
+    EXPECT_CALL(*mocked_usart, usart_recv_blocking_invoke(USART2)).Times(1);
+
+    uart_handler.receive();
+}
+
+
+/***** Uart_Handler::setup() *****/
+
+class UartHandlerSetup : public ::testing::Test {
+protected:
+    virtual void SetUp() {
+        mocked_usart = new MockUsart();
+    }
+
+    virtual void TearDown() {
+        delete mocked_usart;
+    }
+};
+
+
+TEST_F(UartHandlerSetup, Setup) {
+    Uart_Handler uart_handler;
+
+    EXPECT_CALL(*mocked_usart, usart_set_baudrate_invoke(USART2, 115200)).Times(1);
+    EXPECT_CALL(*mocked_usart, usart_set_databits_invoke(USART2, 8)).Times(1);
+    EXPECT_CALL(*mocked_usart, usart_set_stopbits_invoke(USART2, USART_STOPBITS_1)).Times(1);
+    EXPECT_CALL(*mocked_usart, usart_set_mode_invoke(USART2, USART_MODE_TX_RX)).Times(1);
+    EXPECT_CALL(*mocked_usart, usart_set_parity_invoke(USART2, USART_PARITY_NONE)).Times(1);
+    EXPECT_CALL(*mocked_usart, usart_set_flow_control_invoke(USART2, USART_FLOWCONTROL_NONE)).Times(1);
+    EXPECT_CALL(*mocked_usart, usart_enable_invoke(USART2)).Times(1);
+
+    uart_handler.setup();
+}
